0a65cdd69cfcdda08d17c809f499efca66097763
Samar <osmansamar367@gmail.com>
2025-01-21 17:08:50 +0100

    Updated

---
+++ contact.php
@@ -0,0 +1,20 @@
+<?php get_header();
+
+//Template Name: contact
+?>
+
+<main class="contact flex-grow-1">
+    <div>
+        Hiiiiiiiiiiiiiiiiiiiiiiii
+        <!-- 11 -->
+        <!-- Form Section container  -->
+        <?php get_template_part('components/form') ?>
+        <!-- End of Form Section -->
+    </div>
+
+
+</main>
+
+
+
+<?php get_footer() ?>
